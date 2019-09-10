0627d5b64b6fce8665fd6f98a94004a7478caa87
Shadesfear <chris@thecarmans.com>
2019-09-10 16:00:31 +0200

    changes

--- klub100_generator/klub100_generator/urls.py
+++ klub100_generator/klub100_generator/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('klub100/', include('klub100.urls')),
+    path('', include('klub100.urls')),
     path('admin/', admin.site.urls),
 ]
